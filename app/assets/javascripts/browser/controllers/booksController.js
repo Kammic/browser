13a5315de5329b769a3c8aa0137dbba0b4f00f51
Sumeet Singh <ortuna@gmail.com>
2013-11-08 16:50:20 -0500

    Working with temp example

--- app/assets/javascripts/browser/controllers/booksController.js
+++ app/assets/javascripts/browser/controllers/booksController.js
@@ -1,3 +1,7 @@
-BooksController = ["$scope", "$rootScope", function($scope, $rootScope) {
+BooksController = ["$scope", "$rootScope", "$http", function($scope, $rootScope, $http) {
   $scope.name  = "BooksController";
+  $http({method: 'GET', url: '/books'}).
+    success(function(data, status, headers, config) {
+      $scope.books = data;
+    })
 }];
