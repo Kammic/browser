4cbbfd9d669a2bd2bb7438504646fded53f0dde2
Sumeet Singh <ortuna@gmail.com>
2013-11-08 22:07:18 -0500

    Cleanup

--- app/assets/javascripts/browser.js
+++ app/assets/javascripts/browser.js
@@ -4,6 +4,7 @@
 
 //= require ./lib/angular/angular.min
 //= require ./lib/angular/angular-route.min
+//= require ./lib/angular/angular-sanitize.min
 //= require ./browser/application
 
 //= require_tree ./browser
--- app/assets/javascripts/browser/application.js
+++ app/assets/javascripts/browser/application.js
@@ -1,2 +1,19 @@
 var Browser = angular.module('Browser',  ['ngRoute']);
 
+Browser.config(['$routeProvider',
+               function($routeProvider) {
+                 $routeProvider.
+                   when('/books', {
+                   templateUrl: 'books/index.html',
+                   controller: 'BooksController'
+                 }).
+                   when('/phones/:phoneId', {
+                   templateUrl: 'partials/phone-detail.html',
+                   controller: 'PhoneDetailCtrl'
+                 }).
+                   otherwise({
+                   redirectTo: '/books'
+                 });
+               }]);
+
+
